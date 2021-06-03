a3effbefa736f47999cbe98b2abe06f63d57979b
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-06-04 02:59:43 +0700

    cqrs

---
+++ src/Services/Ordering/Application/Contracts_Interfaces/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts_Interfaces {
+    public interface IEmailService {
+        Task<bool> SendMail(IEmail email);
+    }
+
+    public interface IEmail {
+        string To { get; set; }
+        string Subject { get; set; }
+        string Body { get; set; }
+    }
+}
---
+++ src/Services/Ordering/Application/Contracts_Interfaces/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts_Interfaces {
+    public interface IOrderRepository : IRepository<Order> {
+        Task<IEnumerable<Order>> GetAllOrdersByUserId(string userId);
+    }
+}
